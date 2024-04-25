a1f0b8ed3621f9df57b514abe9b93efe5f00beec
stottse <sto17018@byui.edu>
2024-04-24 19:13:13 -0600

    finished teach activity one

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -7,10 +7,25 @@ public static class ArraySelector
         var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
+    
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return new int[0];
+        var list1Index = 0;
+        var list2Index = 0;
+        var result = new int[select.Length]; 
+        for (var i = 0; i< select.Length; i++ )
+        {
+            if (select[i] == 1)
+                result[i] = list1[list1Index++];
+            else
+                result[i] = list2[list2Index++]; 
+
+            
+
+
+        }
+        return result;
     }
 }
