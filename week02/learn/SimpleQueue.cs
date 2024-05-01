cd775403832dd0080829b51c54741210d85aef89
stottse <sto17018@byui.edu>
2024-05-01 13:23:11 -0600

    Finished the week 2 learning activities

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); // I had to change this from insert to add because it just needs to be added at the end of the queue
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; //Lists do not start at 1, they start at 0
+        _queue.RemoveAt(0);
         return value;
     }
 }
